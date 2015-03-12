eb260775919e579db0854174401d5c15784fade3
unknown <S0075724@P0TLS594.itgce.caisse-epargne.fr>
2015-03-12 14:49:03 +0100

    Initialisation du projet sur seconde machine

--- dontforget-server/conf/evolutions/default/1.sql
+++ dontforget-server/conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 # --- !Ups
 
 create table T_PLACE (
-  id_place                  integer auto_increment not null,
+  id_place                  integer not null,
   idUser                    integer,
   place                     varchar(255),
   constraint uq_T_PLACE_1 unique (idUser,place),
@@ -12,7 +12,7 @@ create table T_PLACE (
 ;
 
 create table T_TAG (
-  id_tag                    integer auto_increment not null,
+  id_tag                    integer not null,
   idUser                    integer,
   tag                       varchar(255),
   constraint uq_T_TAG_1 unique (idUser,tag),
@@ -20,14 +20,14 @@ create table T_TAG (
 ;
 
 create table T_TASK (
-  idTask                    integer auto_increment not null,
+  idTask                    integer not null,
   idUser                    integer,
   text                      varchar(5000) not null,
   constraint pk_T_TASK primary key (idTask))
 ;
 
 create table T_USER (
-  idUser                    integer auto_increment not null,
+  idUser                    integer not null,
   pseudo                    varchar(32) not null,
   password                  varchar(40) not null,
   dateInscription           bigint not null,
@@ -46,6 +46,14 @@ create table T_TASK_PLACE (
   T_PLACE_id_place               integer not null,
   constraint pk_T_TASK_PLACE primary key (T_TASK_idTask, T_PLACE_id_place))
 ;
+create sequence T_PLACE_seq;
+
+create sequence T_TAG_seq;
+
+create sequence T_TASK_seq;
+
+create sequence T_USER_seq;
+
 alter table T_PLACE add constraint fk_T_PLACE_user_1 foreign key (idUser) references T_USER (idUser) on delete restrict on update restrict;
 create index ix_T_PLACE_user_1 on T_PLACE (idUser);
 alter table T_TAG add constraint fk_T_TAG_user_2 foreign key (idUser) references T_USER (idUser) on delete restrict on update restrict;
@@ -65,19 +73,27 @@ alter table T_TASK_PLACE add constraint fk_T_TASK_PLACE_T_PLACE_02 foreign key (
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table T_PLACE;
+drop table if exists T_PLACE;
 
-drop table T_TAG;
+drop table if exists T_TAG;
 
-drop table T_TASK;
+drop table if exists T_TASK;
 
-drop table T_TASK_TAG;
+drop table if exists T_TASK_TAG;
 
-drop table T_TASK_PLACE;
+drop table if exists T_TASK_PLACE;
 
-drop table T_USER;
+drop table if exists T_USER;
 
-SET FOREIGN_KEY_CHECKS=1;
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists T_PLACE_seq;
+
+drop sequence if exists T_TAG_seq;
+
+drop sequence if exists T_TASK_seq;
+
+drop sequence if exists T_USER_seq;
 
